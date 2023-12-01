20d2c925044b0b72e3f059e079ced3ec74e6ad13
VVSiva <VeeraSivaVijay@gmail.com>
2023-12-01 06:17:20 +0530

    <<!!CHANGES!!>>

--- Animal_Taxonomy_Tk.py
+++ Animal_Taxonomy_Tk.py
@@ -106,7 +106,11 @@ def home_page():
         if value == "name":
             for row in cur.execute("SELECT name, kingdom, phylum, class, naturalorder, family, genus, species FROM animal_details WHERE  name = '"+tosearch+"' "):
                 label = tk.Label(home_frame, text = row, font = ("Arial" , 10, "italic" ), bg = "darkgrey")
-                label.place(x = 1, y = 180)             
+                label.place(x = 1, y = 180) 
+            #if cur.execute("SELECT EXISTS"("SELECT name, kingdom, phylum, class, naturalorder, family, genus, species FROM animal_details WHERE  name = '"+tosearch+"' ")) == False:  
+            #    label = tk.Label(home_frame, text = "No Records", font = ("Arial" , 10, "italic" ), bg = "darkgrey")
+            #    label.place(x = 1, y = 180) 
+
         elif value == "kingdom":
             for row in cur.execute("SELECT name, kingdom, phylum, class, naturalorder, family, genus, species FROM animal_details WHERE  kingdom = '"+tosearch+"' "):
                 label = tk.Label(home_frame, text = row, font = ("Arial" , 10, "italic" ), bg = "darkgrey")
